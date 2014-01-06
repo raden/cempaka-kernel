9ae6380522bc23608ce7d54814ad6778b32d0950
Chester Kener <Cl3Kener@gmail.com>
2014-01-05 20:04:09 -0500

    cpufreq: Add Many Governors
    * Look below to see everything I added :)
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    cpufreq: InteractiveX CPU governor
    
    Modified version of interactive with sleep+wake code
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    cpufreq: OnDemandX CPU governor
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    cpufreq: Lionheart CPU governor
    
    A brave and agile conservative-based governor
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    cpufreq: SmartAssV2 CPU governor
    
    A smart optimized governor based on OnDemand
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    cpufreq: LulzActive CPU governor
    
    A new interactive governor developed by Tegrak.
    For more information please visit:
    http://tegrak2x.blogspot.com/2011/11/lulzactive-governor-v2.html
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    cpufreq: Adaptive CPU governor
    
    This driver adds a dynamic cpufreq policy governor designed for
    latency-sensitive workloads and also for demanding performance.
    This governor attempts to reduce the latency of clock increases so
    that the system is more responsive to interactive workloads in loweset
    steady-state but to reduce power consumption in middle operation level
    up will be done in step by step to prohibit system from going to max
    operation level.
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    cpufreq: Hyper CPU governor
    
    A tweaked OnDemand based smart and smooth optimized governor
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    cpufreq: BrazilianWax CPU governor
    
    A slightly more agressive smart optimized governor
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    cpufreq: Intellidemand CPU governor
    
    This is an 'ondemand' based CPU governor created by Faux123 (thanks to him for his awesome work)
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    Fix
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	drivers/cpufreq/cpufreq_intellidemand.c
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -372,9 +372,21 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
+extern struct cpufreq_governor cpufreq_gov_ondemandx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
+extern struct cpufreq_governor cpufreq_gov_adaptive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_adaptive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BRAZILIANWAX)
+extern struct cpufreq_governor cpufreq_gov_brazilianwax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_brazilianwax)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
+extern struct cpufreq_governor cpufreq_gov_hyper;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hyper)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
@@ -384,6 +396,21 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor 		cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor 		cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
+extern struct cpufreq_governor 		cpufreq_gov_lulzactive;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lulzactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor 		cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor 		cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellidemand)
 #endif
 
 
