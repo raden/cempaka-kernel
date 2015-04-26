238dabbab17c8741cfc4c6b061ba361f726ca1ce
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2015-04-26 10:12:50 +0800

    Cempaka v1.0 MR1
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

---
+++ bina-stok.sh
@@ -0,0 +1,24 @@
+export ARCH=arm
+#unset CROSS_COMPILE
+#export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
+#export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2014.12/bin/arm-cortex_a15-linux-gnueabihf-
+
+#echo "Cleaning old craps..."
+#make distclean
+
+#export CROSS_COMPILE=/opt/old-linaro/arm-linux-gnueabi-linaro_4.7.4-2014.01/bin/arm-linux-gnueabi-
+#export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
+#export CROSS_COMPILE=/opt/old-linaro/arm-linux-gnueabi-linaro_4.8.3-2014.02/bin/arm-linux-gnueabi-
+export KBUILD_BUILD_USER=najmi
+export KBUILD_BUILD_HOST="ampang"
+export LOCALVERSION="-Cempaka-v1.0-mr1"
+
+#echo "Copy backup config..."
+#cp najmi-mako-config .config
+#make cyanogen_mako_defconfig
+#make menuconfig
+#echo "Begin compile..."
+#make -j8
+
+
